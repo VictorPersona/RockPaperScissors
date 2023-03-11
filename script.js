14fbcf6d860a252f7c12cdd9837f438722525c9c
VictorPersona <gauravtupe00@gmail.com>
2023-03-11 23:12:25 +0530

    first

--- script.js
+++ script.js
@@ -1,68 +1,89 @@
-function playerSelection(){
-     var choices =["Rock", "Paper", "Scissors"];
-     let move =  choices[Math.floor((Math.random())*choices.length)];
-     return prompt("Throw Your Move",move);
+function computerSelection() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  return choices[Math.floor(Math.random() * 3)];
 }
 
-function computerSelection(){
-     const choices =["Rock", "Paper", "Scissors"];
-    return choices[Math.floor((Math.random())*3)];
-}
+function playRound(player, computer) {
+     let result;
+  if (player === computer) {
+    result=("Both threw", player, "It is Tie");
+  } else if ((player === "rock", "Rock", "ROCK")) {
+    if (computer === "Paper") {
+      result=("Computer threw", computer, "You lose");
+       ++computerScore;
+    } else if (computer === "Scissors") {
+      result=("Computer threw", computer, "You Won");
+       ++playerScore;
+    } else if (computer === "Rock") {
+      result=("Computer threw", computer, "It is Tie");
+    }
+  } else if ((player === "Paper", "PAPER", "paper")) {
+    if (computer === "Paper") {
+      result=("Computer threw", computer, "It is Tie");
+    } else if (computer === "Scissors") {
+      result=("Computer threw", computer, "You Lost");
+       ++computerScore;
+    } else if (computer === "Rock") {
+      result=("Computer threw", computer, "You Won");
+       ++playerScore;
+    }
+  } else if ((player === "Scissors", "SCISSORS", "scissors")) {
+    if (computer === "Paper") {
+      result=("Computer threw", computer, "You Won");
+       ++playerScore;
+    } else if (computer === "Scissors") {
+      result=("Computer threw", computer, "It is Tie");
+    } else if (computer === "Rock") {
+      result=("Computer threw", computer, "You Lost");
+       ++computerScore;
+    }
+}const consoleElement = document.getElementById("consoleElement");
+  consoleElement.textContent = result;
 
-function playRound(player,computer){
-if(player === computer){
-     console.log("Both threw",player,"It is Tie")
-}
-else if(player === "rock", "Rock", "ROCK"){
-          if (computer === "Paper"){
-               console.log("Computer threw",computer,"You lose");
-               return ++computerScore
-          }
-          else if (computer === "Scissors"){
-               console.log("Computer threw",computer,"You Won");
-               return ++playerScore
-          }
-          else if (computer === "Rock"){
-               console.log("Computer threw",computer,"It is Tie");
-          }
-     }
- else if(player ===  "Paper", "PAPER", "paper"){
-          if (computer === "Paper"){
-               console.log("Computer threw",computer,"It is Tie");
-          }
-          else if (computer === "Scissors"){
-               console.log("Computer threw",computer,"You Lost");
-               return ++computerScore
-               
-          }
-          else if (computer === "Rock"){
-               console.log("Computer threw",computer,"You Won");
-               return ++playerScore
-          }
-}
-else if(player === "Scissors", "SCISSORS", "scissors"){
-     if (computer === "Paper"){
-          console.log("Computer threw",computer,"You Won");
-          return ++playerScore
-     }
-     else if (computer === "Scissors"){
-          console.log("Computer threw",computer,"It is Tie");
-     }
-     else if (computer === "Rock"){
-          console.log("Computer threw",computer,"You Lost");
-          return ++computerScore
-     }
-}
+  const resultElement = document.getElementById("result");
+  resultElement.textContent = result;
 }
 var playerScore = 0;
 var computerScore = 0;
 
-function game(){
-     for (let i = 0; i < 5; i++) {
-          playRound(playerSelection(),computerSelection())
-          console.log("Your Score is ",playerScore,"Computer's Score is",computerScore );
-          console.log("----------------------------------------------");
-       }
+function game() {
+  playRound(playerSelection, computerSelection());
+  document.getElementById("scoreBoard").innerHTML = `
+  <p>Your Score is ${playerScore}</p>
+  <p>Computer's Score is ${computerScore}</p>
+  <hr>
+`;
 }
 
-console.log(game());
+const container = document.querySelector("#container");
+
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissors = document.createElement("button");
+
+rock.classList.add("content");
+rock.textContent = "Rock";
+container.appendChild(rock);
+rock.addEventListener("click", () => {
+  playerSelection = "Rock";
+  game();
+});
+
+paper.classList.add("content");
+paper.textContent = "Paper";
+container.appendChild(paper);
+paper.addEventListener("click", () => {
+  playerSelection = "Paper";
+  game();
+});
+
+scissors.classList.add("content");
+scissors.textContent = "Scissors";
+container.appendChild(scissors);
+scissors.addEventListener("click", () => {
+  playerSelection = "Scissors";
+  game();
+});
+
+
+
